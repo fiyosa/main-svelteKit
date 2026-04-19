a6c18fc483e249121573ef3dd62eba0c7f3d65ae
fiyosa <fiyosa97@gmail.com>
2026-04-19 10:36:29 +0700

    add: deployment v2

--- deploy.sh
+++ deploy.sh
@@ -2,16 +2,16 @@
 
 pnpm build
 
-docker build -t portfolio_image .
-docker stop portfolio
-docker rm portfolio
+docker build -t portfolio .
+docker stop portfolio 2>/dev/null || true
+docker rm portfolio 2>/dev/null || true
 
 docker run -d \
   --name portfolio \
   --restart always \
   -p 8000:8000 \
   --env-file /root/docker/portfolio/.env \
-  portfolio_image
+  portfolio
 
 docker image prune -f
 
